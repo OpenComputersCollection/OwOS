01fbb70d2a258bfaaed9456762ec5aa77a001e63
Crafty-Codes <fabio.oberhofer@gmx.net>
2023-05-06 20:22:50 +0200

    src: ip is now from dhcp

--- boot/95_network.lua
+++ boot/95_network.lua
@@ -1,13 +1,21 @@
 local fs = require("filesystem")
+local component = require("component")
+local thread = require("thread")
+local event  = require "event"
+
 require "file"
 
 local network_path = "/etc/network/"
 local file_name = "interface"
+local port = 11
+local port_extern = 10
+local message_word = "getIp"
+local timeout = 10
 
-local ip = "10.0.0.10"
 
 local config = {
-    ip = ip
+    gateway = nil,
+    ip = nil
 }
 
 if not fs.exists(network_path) then
@@ -17,4 +25,29 @@ if not fs.exists(network_path) then
     end
 end
 
-write_config(network_path .. file_name, "network", config)
+component.modem.open(port)
+
+local t = thread.create(function() 
+
+    while not config.ip do
+        component.modem.broadcast(port_extern, message_word)
+
+        _, _, id, _, _, message = event.pull(timeout, "modem_message")
+
+        if message then
+            config.ip = message
+            config.gateway = id
+        end
+    end
+
+end):detach()
+
+if t then
+    t:join()
+
+    write_config(network_path .. file_name, "network", config) 
+else 
+    error("Get ip thread died abruptly")
+end
+
+component.modem.close(port)
